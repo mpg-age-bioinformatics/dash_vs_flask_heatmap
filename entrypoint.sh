18c3aa2f766641cb28b5c97feb508a289e60cafb
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-06-09 15:51:21 +0200

    Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ "$FLASK_ENV" == "production" ]] ; 
   then
-    gunicorn -b 0.0.0.0:8000 --timeout 60000 -w ${N_WORKERS} hetmap:app
+    gunicorn -b 0.0.0.0:8000 --timeout 60000 -w ${N_WORKERS} heatmap:app
 elif [[ "$FLASK_ENV" == "development" ]] ; 
   then
     tail -f /dev/null
